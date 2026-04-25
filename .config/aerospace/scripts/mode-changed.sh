9eaaccbc55ad84323a1981661cffdcecb3f17300
arham-lodha0317 <32276821+arham-lodha@users.noreply.github.com>
2026-04-25 03:21:12 -0500

    Update dotfiles and remove secret configs

--- .config/aerospace/scripts/mode-changed.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-osascript -e "display notification \"Entered ${AEROSPACE_MODE} mode\" with title \"AeroSpace\""
--- .config/zsh/.zprofile
+++
@@ -1,3 +0,0 @@
-eval "$(/opt/homebrew/bin/brew shellenv)"
-
-export PATH="$HOME/.elan/bin:$PATH"
